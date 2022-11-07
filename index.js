05e4083b93334250a5da6ae5f23a27137ef60297
arjun gupta <arjunkarangupta100@gmail.com>
2022-11-06 22:22:33 -0500

    added questions and now figureing out how to combine the user input into a read me file, I know I have to append it

--- index.js
+++ index.js
@@ -1,25 +1,76 @@
 // TODO: Include packages needed for this application
 //Installed Inquirer, it was already there
+// TODO: Create an array of questions for user input
+// const questions = ['What is the title of your project?',
+//     'Give a Description of your project:',
+// ];
+
+const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = ['What is the title of your project?',
-    'Give a Description of your project:',
-];
+const thisIsIt = () => {
+    fs.appendFile('README.md', `${name.title}\n`, (err) =>
+        err ? console.error(err) : console.log('Heres a read me')
+    
+    );
+    }
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-fs.writeFile('README.md', process.argv[2], (err) =>
-    err ? console.error(err) : console.log('Heres a read me')
+inquirer
+    .prompt([
+    {
+      type: 'confirm',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    // {
+    //   type: 'input',
+    //   message: 'Description section of your project:',
+    //   name: 'description',
+    // },
+    // {
+    //     type: 'confirm',
+    //     message: 'Would you like a table of contents?',
+    //     name: 'tableOC',
+    //   },
+    //   {
+    //     type: 'confirm',
+    //     message: 'instillation section:',
+    //     name: 'installSection',
+    //   },
+    //   {
+    //     type: 'confirm',
+    //     message: 'Would you like to include usage information?',
+    //     name: 'usageInfo',
+    //   },
+    //   {
+    //     type: 'confirm',
+    //     message: 'Add to contribution guidlines?',
+    //     name: 'contGL',
+    //   },  
+    // {
+    //   type: 'input',
+    //   message: 'Test Instructions:',
+    //   name: 'testInstructions',
+    // },
+  ])
+  .then((response) =>
+    response.title === true
+      ? thisIsIt()
+      : console.log('Bruh')
+  );
 
-);
 
-//  }
+// // TODO: Create a function to write README file
+// // function writeToFile(fileName, data) {
 
-// TODO: Create a function to initialize app
-function init() {
+// // THE \n is to put the input on the next line in the file
 
-}
+// //  }
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {
+
+// }
+
+// // Function call to initialize app
+// init();
